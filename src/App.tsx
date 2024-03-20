78ddb9a8e9903939b095663b9f914453b239bc9f
Peter Lazar <peter.lazar@limehome.de>
2024-03-20 16:43:04 +0100

    add build dir

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const colorKeys = ["Primary",
 
 const Content = colorKeys.map(parent => {
   const colors = Object.keys(variables[parent])
+  //@ts-expect-error FIX ME
   return <div style={{ display: 'flex', marginTop: 10 }} key={parent}><h2 style={{ width: 200 }}>{parent}</h2>{colors.map((color) => { return (<div key={parent + color} style={{ width: 100, height: 100, marginLeft: 10, background: variables[parent][color], }}><h4 style={{ color: 'black' }}>{color}</h4></div>) })}</div>
 })
 
